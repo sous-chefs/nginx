b5d2bf3ba4e150af578faa50c2d92af68308c73e
Lucas Jandrew <ljandrew@riotgames.com>
2012-07-31 19:12:41 -0700

    Namespaced to riot_nginx

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,7 +22,7 @@ include_recipe 'nginx::ohai_plugin'
 
 case node['nginx']['install_method']
 when 'source'
-  include_recipe 'nginx::source'
+  include_recipe 'riot_nginx::source'
 when 'package'
   case node['platform']
   when 'redhat','centos','scientific','amazon','oracle'
@@ -33,7 +33,7 @@ when 'package'
     supports :status => true, :restart => true, :reload => true
     action :enable
   end
-  include_recipe 'nginx::commons'
+  include_recipe 'riot_nginx::commons'
 end
 
 service 'nginx' do
