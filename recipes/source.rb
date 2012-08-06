a1f84d0acc92aca23114ad17a2db12fd2489eb4e
Danial Pearce <git@tigris.id.au>
2012-08-06 18:14:32 +1000

    Addition fix for first run error: COOK-1522

--- recipes/source.rb
+++ recipes/source.rb
@@ -98,6 +98,7 @@ bash "compile_nginx_source" do
   not_if do
     nginx_force_recompile == false &&
       node.automatic_attrs &&
+      node.automatic_attrs['nginx'] &&
       node.automatic_attrs['nginx']['version'] == node['nginx']['version'] &&
       node.automatic_attrs['nginx']['configure_arguments'].sort == configure_flags.sort
   end
