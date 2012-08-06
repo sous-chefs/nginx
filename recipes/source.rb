488651593bee8b89bb16a30770a2a7abde7b4d55
Danial Pearce <git@tigris.id.au>
2012-08-06 17:13:20 +1000

    Add init style startup scripts for debian/ubuntu: COOK-1484

--- recipes/source.rb
+++ recipes/source.rb
@@ -143,7 +143,7 @@ when "bluepill"
   end
 else
   node.set['nginx']['daemon_disable'] = false
-  
+
   template "/etc/init.d/nginx" do
     source "nginx.init.erb"
     owner "root"
@@ -158,7 +158,13 @@ else
     )
   end
 
-  template "/etc/sysconfig/nginx" do
+  defaults_path = case node['platform']
+    when 'debian', 'ubuntu'
+      '/etc/default/nginx'
+    else
+      '/etc/sysconfig/nginx'
+  end
+  template defaults_path do
     source "nginx.sysconfig.erb"
     owner "root"
     group "root"
