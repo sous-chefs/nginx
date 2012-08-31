12dffd1effb78be0a99622b998878b54509cf012
jtimberman <joshua@opscode.com>
2012-08-31 10:37:45 -0600

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures nginx"
-version           "0.101.6"
+version           "0.101.7"
 
 recipe "nginx", "Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available"
 recipe "nginx::source", "Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available"
