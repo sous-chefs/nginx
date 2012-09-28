e12fbda91c76d9b6cd7a4dd8d0a2db5bb771785c
Lloyd Philbrook <lloyd@firebellydesign.com>
2012-09-28 12:46:30 +0800

    added nginx group

--- attributes/default.rb
+++ attributes/default.rb
@@ -41,6 +41,8 @@ else
   default['nginx']['init_style'] = "init"
 end
 
+default['nginx']['group'] = default['nginx']['user']
+
 default['nginx']['pid'] = "/var/run/nginx.pid"
 
 default['nginx']['gzip']              = "on"
