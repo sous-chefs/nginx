6fc748b864e0167d91321d50256c0ca151e3e4e6
Julian C. Dunn <jdunn@secondmarket.com>
2012-10-14 00:04:03 -0400

    COOK-1770 Check that the system has a 'cpu' attribute before setting the # of workers. If not, assume 1.

--- attributes/default.rb
+++ attributes/default.rb
@@ -61,7 +61,7 @@ default['nginx']['gzip_types']        = [
 
 default['nginx']['keepalive']          = "on"
 default['nginx']['keepalive_timeout']  = 65
-default['nginx']['worker_processes']   = cpu['total']
+default['nginx']['worker_processes']   = node['cpu'] ? node['cpu']['total'] : 1
 default['nginx']['worker_connections'] = 1024
 default['nginx']['server_names_hash_bucket_size'] = 64
 
