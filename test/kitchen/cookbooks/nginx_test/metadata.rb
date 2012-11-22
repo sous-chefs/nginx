207f68b53cc89352c7c2401c571001040798d80a
jtimberman <joshua@opscode.com>
2012-11-21 18:02:06 -0700

    add configurations and recipes for test cookbook

---
+++ test/kitchen/cookbooks/nginx_test/metadata.rb
@@ -0,0 +1,6 @@
+name              "nginx"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "This cookbook is used with test-kitchen to test the parent, nginx cookbok"
+version           "1.0.0"
