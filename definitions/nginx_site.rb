a567ec2585f906bf788dc8ea1390db9641cbf1b4
jtimberman <joshua@opscode.com>
2012-11-25 15:17:36 -0700

    missed definition: use updated notification syntax for service

--- definitions/nginx_site.rb
+++ definitions/nginx_site.rb
@@ -22,13 +22,13 @@ define :nginx_site, :enable => true do
   if params[:enable]
     execute "nxensite #{params[:name]}" do
       command "/usr/sbin/nxensite #{params[:name]}"
-      notifies :reload, resources(:service => "nginx")
+      notifies :reload, "service[nginx]"
       not_if do ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{params[:name]}") end
     end
   else
     execute "nxdissite #{params[:name]}" do
       command "/usr/sbin/nxdissite #{params[:name]}"
-      notifies :reload, resources(:service => "nginx")
+      notifies :reload, "service[nginx]"
       only_if do ::File.symlink?("#{node['nginx']['dir']}/sites-enabled/#{params[:name]}") end
     end
   end
