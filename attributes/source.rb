f1d060bb3e01bb5476e51c16d19d9de7d15e95a3
Panagiotis PJ Papadomitsos <pj@ezgr.net>
2012-12-27 18:20:46 +0200

    update on node source version attribute fix

--- attributes/source.rb
+++ attributes/source.rb
@@ -21,7 +21,8 @@
 
 include_attribute 'nginx'
 
-default['nginx']['source']['prefix']                  = "/opt/nginx-#{node['nginx']['version']}"
+default['nginx']['source']['version']                 = node['nginx']['version']
+default['nginx']['source']['prefix']                  = "/opt/nginx-#{node['nginx']['source']['version']}"
 default['nginx']['source']['conf_path']               = "#{node['nginx']['dir']}/nginx.conf"
 default['nginx']['source']['sbin_path']               = "#{node['nginx']['source']['prefix']}/sbin/nginx"
 default['nginx']['source']['default_configure_flags'] = [
