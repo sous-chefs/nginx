e168d08c513c29beeab91918832c79b3305fe253
Tim Smith <tsmith@llnw.com>
2013-01-31 10:04:17 -0800

    Update the checksums for the current files

--- attributes/geoip.rb
+++ attributes/geoip.rb
@@ -22,9 +22,9 @@
 default['nginx']['geoip']['path']                 = "/srv/geoip"
 default['nginx']['geoip']['enable_city']          = true
 default['nginx']['geoip']['country_dat_url']      = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz"
-default['nginx']['geoip']['country_dat_checksum'] = "bbd5ea2bf1de800237a56ea0600f3d8ede2e2956937a8e632118f397af75adfa"
+default['nginx']['geoip']['country_dat_checksum'] = "40865af8f49b9898957cb9b81548676ecb2efd6073a0a437a7b2afcc594bf8fe"
 default['nginx']['geoip']['city_dat_url']         = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
-default['nginx']['geoip']['city_dat_checksum']    = "097f74d8295f82ca256d522497c3a105aaa6a353260c5d2c084156b29a54d431"
+default['nginx']['geoip']['city_dat_checksum']    = "2db87dd2ed665833b71b5f330476ad850cf3b34001f101d581c8e470dba50a5f"
 default['nginx']['geoip']['lib_version']          = "1.4.8"
 default['nginx']['geoip']['lib_url']              = "http://geolite.maxmind.com/download/geoip/api/c/GeoIP-#{node['nginx']['geoip']['lib_version']}.tar.gz"
 default['nginx']['geoip']['lib_checksum']         = "cf0f6b2bac1153e34d6ef55ee3851479b347d2b5c191fda8ff6a51fab5291ff4"
