a8426e73fb79c081106fb39c8c0f34e6e8597a90
Allan Espinosa <allan.espinosa@outlook.com>
2013-05-02 00:01:22 +0900

    Cover all pending specs

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,19 +1,62 @@
 require 'chefspec'
 
 describe 'nginx::default' do
-  it "loads the ohai plugin"
+  before do
+    # stub out ohai
+    Chef::Config[:config_file] = '/dev/null'
+  end
+  let(:runner) do
+    ChefSpec::ChefRunner.new(platform: 'debian')
+  end
+  let(:chef_run) do
+    runner.converge 'nginx::default'
+  end
 
-  it "builds from source when specified"
+  it "loads the ohai plugin" do
+    expect(chef_run).to include_recipe 'nginx::ohai_plugin'
+  end
+
+  it "builds from source when specified" do
+    # needed to converge nginx::source
+    runner.node.set[:nginx][:init_style] = 'none'
+    runner.node.set[:nginx][:install_method] = 'source'
+    expect(chef_run).to include_recipe 'nginx::source'
+  end
 
   context "configured to install by package" do
     context "in a redhat-based platform" do
-      it "includes the yum::epel recipe if the source is epel"
-      it "includes the nginx::repo recipe if the source is not epel"
+      let(:redhat) do
+        ChefSpec::ChefRunner.new(platform: 'redhat')
+      end
+      let(:redhat_run) do
+        redhat.converge 'nginx::default'
+      end
+      it "includes the yum::epel recipe if the source is epel" do
+        redhat.node.set[:nginx][:repo_source] = 'epel'
+        expect(redhat_run).to include_recipe 'yum::epel'
+      end
+        
+      it "includes the nginx::repo recipe if the source is not epel" do
+        redhat.node.set[:nginx][:repo_source] = 'notepel'
+        expect(redhat_run).to include_recipe 'nginx::repo'
+      end
     end
-    it "installs the package"
-    it "enables the service"
-    it "executes common nginx configuration"
+
+    it "installs the package" do
+      package_name = chef_run.node[:nginx][:package_name]
+      expect(chef_run).to install_package package_name
+    end
+
+    it "enables the service" do
+      expect(chef_run).to enable_service 'nginx'
+    end
+
+    it "executes common nginx configuration" do
+      expect(chef_run).to include_recipe 'nginx::commons'
+    end
   end
 
-  it "starts the service"
+  it "starts the service" do
+    expect(chef_run).to start_service 'nginx'
+  end
 end
