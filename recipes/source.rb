2ae0eb644649eae46428d372c8c58baa14f4f022
Seth Chisamore <schisamo@opscode.com>
2013-05-02 00:05:30 -0400

    [COOK-2409] move service declaration to end of recipe
    Previously the new runit_service resource would try to restart before 
    the nginx code is compiled and available. This has the nasty side effect 
    of causing the Chef run to fail!

--- recipes/source.rb
+++ recipes/source.rb
@@ -64,6 +64,42 @@ node.run_state['nginx_force_recompile'] = false
 node.run_state['nginx_configure_flags'] =
   node['nginx']['source']['default_configure_flags'] | node['nginx']['configure_flags']
 
+include_recipe "nginx::commons_conf"
+
+cookbook_file "#{node['nginx']['dir']}/mime.types" do
+  source "mime.types"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :reload, 'service[nginx]'
+end
+
+node['nginx']['source']['modules'].each do |ngx_module|
+  include_recipe "nginx::#{ngx_module}"
+end
+
+configure_flags = node.run_state['nginx_configure_flags']
+nginx_force_recompile = node.run_state['nginx_force_recompile']
+
+bash "compile_nginx_source" do
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    tar zxf #{::File.basename(src_filepath)} -C #{::File.dirname(src_filepath)} &&
+    cd nginx-#{node['nginx']['source']['version']} &&
+    ./configure #{node.run_state['nginx_configure_flags'].join(" ")} &&
+    make && make install
+  EOH
+
+  not_if do
+    nginx_force_recompile == false &&
+      node.automatic_attrs['nginx'] &&
+      node.automatic_attrs['nginx']['version'] == node['nginx']['source']['version'] &&
+      node.automatic_attrs['nginx']['configure_arguments'].sort == configure_flags.sort
+  end
+
+  notifies :restart, "service[nginx]"
+end
+
 case node['nginx']['init_style']
 when "runit"
   node.set['nginx']['src_binary'] = node['nginx']['binary']
@@ -162,42 +198,6 @@ else
     supports :status => true, :restart => true, :reload => true
     action :enable
   end
-end
-
-include_recipe "nginx::commons_conf"
-
-cookbook_file "#{node['nginx']['dir']}/mime.types" do
-  source "mime.types"
-  owner "root"
-  group "root"
-  mode 00644
-  notifies :reload, 'service[nginx]'
-end
-
-node['nginx']['source']['modules'].each do |ngx_module|
-  include_recipe "nginx::#{ngx_module}"
-end
-
-configure_flags = node.run_state['nginx_configure_flags']
-nginx_force_recompile = node.run_state['nginx_force_recompile']
-
-bash "compile_nginx_source" do
-  cwd ::File.dirname(src_filepath)
-  code <<-EOH
-    tar zxf #{::File.basename(src_filepath)} -C #{::File.dirname(src_filepath)} &&
-    cd nginx-#{node['nginx']['source']['version']} &&
-    ./configure #{node.run_state['nginx_configure_flags'].join(" ")} &&
-    make && make install
-  EOH
-
-  not_if do
-    nginx_force_recompile == false &&
-      node.automatic_attrs['nginx'] &&
-      node.automatic_attrs['nginx']['version'] == node['nginx']['source']['version'] &&
-      node.automatic_attrs['nginx']['configure_arguments'].sort == configure_flags.sort
-  end
-
-  notifies :restart, "service[nginx]"
 end
 
 node.run_state.delete('nginx_configure_flags')
