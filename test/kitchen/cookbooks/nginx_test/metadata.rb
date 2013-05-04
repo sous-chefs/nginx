cfc3ad6233320eed3bfa3350d96a98550b065255
jtimberman <joshua@opscode.com>
2013-05-03 23:00:09 -0600

    [COOK-2877] - remove test cookbook
    Testing specific configurations will be handled by "suites".
    
    Hopefully w/ COOK-2783, we won't need all the module specific tests anyway.

--- test/kitchen/cookbooks/nginx_test/metadata.rb
+++
@@ -1,6 +0,0 @@
-name              "nginx"
-maintainer        "Opscode, Inc."
-maintainer_email  "cookbooks@opscode.com"
-license           "Apache 2.0"
-description       "This cookbook is used with test-kitchen to test the parent, nginx cookbok"
-version           "1.0.0"
