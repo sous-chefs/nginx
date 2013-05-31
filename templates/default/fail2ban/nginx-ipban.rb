6d4a3d3d469b375b1ca65dda6bfdac096f5394d7
Darrin Eden <darrin.eden@gmail.com>
2013-05-31 09:24:10 -0700

    filter templates

---
+++ templates/default/fail2ban/nginx-ipban.rb
@@ -0,0 +1,8 @@
+# http://serverfault.com/questions/420895/how-to-use-fail2ban-for-nginx
+
+[Definition]
+# Option:  failregex
+# Notes.:  Regexp to catch a generic call from an IP address.
+# Values:  TEXT
+#
+failregex = ^<HOST> -.*"(GET|POST).*HTTP.*"$
