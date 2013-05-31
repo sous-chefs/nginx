1a2f1b48b0403325867ca3972b78837844882d99
Darrin Eden <darrin.eden@gmail.com>
2013-05-30 20:38:53 -0700

    correct file name

--- recipes/fail2ban.rb
+++ recipes/fail2ban.rb
@@ -1,6 +1,6 @@
 include_recipe 'fail2ban'
 
 template '/etc/fail2ban/filter.d/nginx.conf' do
-  source 'fail2ban-nginx.conf.erb'
+  source 'fail2ban.conf.erb'
   notifies :restart, 'service[fail2ban]'
 end
