2b0736f77e8032b45bb4a8038977b8e7473506d6
Matt Wright <matt@nobien.net>
2013-09-09 22:53:28 -0400

    [COOK-3428] Disable logging when `disable_access_log` is true

--- attributes/default.rb
+++ attributes/default.rb
@@ -87,6 +87,8 @@ default['nginx']['server_tokens']      = nil
 default['nginx']['server_names_hash_bucket_size'] = 64
 default['nginx']['sendfile'] = 'on'
 
+default['nginx']['access_log_options'] = nil
+default['nginx']['error_log_options'] = nil
 default['nginx']['disable_access_log'] = false
 default['nginx']['install_method'] = 'package'
 default['nginx']['default_site_enabled'] = true
