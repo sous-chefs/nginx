6c03afe8f81285b12b26de4d8e3468bc15a00fba
Matt Veitas <mveitas@agamatrix.com>
2013-11-03 09:10:03 -0500

    [COOK-3923] - Enable the list of packages installed by the passenger recipe to be configurable

--- attributes/passenger.rb
+++ attributes/passenger.rb
@@ -33,6 +33,9 @@ else
   node.default['nginx']['passenger']['ruby'] = '/usr/bin/ruby'
 end
 
+node.default['nginx']['passenger']['packages']['rhel'] = %w[ruby-devel curl-devel]
+node.default['nginx']['passenger']['packages']['debian'] = %w[ruby-dev libcurl4-gnutls-dev]
+
 node.default['nginx']['passenger']['spawn_method'] = 'smart-lv2'
 node.default['nginx']['passenger']['buffer_response'] = 'on'
 node.default['nginx']['passenger']['max_pool_size'] = 6
