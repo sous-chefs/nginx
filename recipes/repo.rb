ff317972fed0a39bc9483b99e970924923465d1c
Sean OMeara <someara@opscode.com>
2013-12-18 20:08:51 -0500

    removing whitespace

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -20,14 +20,14 @@
 
 case node['platform_family']
 when 'rhel', 'fedora'
-  
+
   yum_repository 'nginx' do
     description 'Nginx.org Repository'
     baseurl         node['nginx']['upstream_repository']
     gpgkey      'http://nginx.org/keys/nginx_signing.key'
     action :create
   end
-  
+
 when 'debian'
   include_recipe 'apt::default'
 
