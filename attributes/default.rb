aa68b47717b2b7d55d4ea7d39b1acb7f649d53e3
Roger Hu <roger.hu@gmail.com>
2014-01-23 12:33:54 -0500

    [COOK-3672] - Add gzip_static option

--- attributes/default.rb
+++ attributes/default.rb
@@ -55,6 +55,7 @@ default['nginx']['group'] = node['nginx']['user']
 default['nginx']['pid'] = '/var/run/nginx.pid'
 
 default['nginx']['gzip']              = 'on'
+default['nginx']['gzip_static']       = 'off'
 default['nginx']['gzip_http_version'] = '1.0'
 default['nginx']['gzip_comp_level']   = '2'
 default['nginx']['gzip_proxied']      = 'any'
