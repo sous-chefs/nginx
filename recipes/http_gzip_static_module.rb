0964378b71b5ce68bd2df01bc439d448f13a0aa9
Roger Hu <roger.hu@gmail.com>
2014-01-23 12:33:54 -0500

    [COOK-3672] - Add gzip_static option

--- recipes/http_gzip_static_module.rb
+++ recipes/http_gzip_static_module.rb
@@ -19,5 +19,12 @@
 # limitations under the License.
 #
 
+template "#{node['nginx']['dir']}/conf.d/http_gzip_static.conf" do
+  source 'modules/http_gzip_static.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
 node.run_state['nginx_configure_flags'] =
   node.run_state['nginx_configure_flags'] | ['--with-http_gzip_static_module']
