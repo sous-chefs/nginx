5bc45131d99f453172a30b45e3359cb7a766bbc3
Sho Sawada <shao@thebitcellar.com>
2014-02-04 17:03:52 +0900

    use prebuilt passenger binary under run with 'nginx::package'

--- recipes/package.rb
+++ recipes/package.rb
@@ -21,11 +21,12 @@
 include_recipe 'nginx::ohai_plugin'
 
 if platform_family?('rhel')
-  if node['nginx']['repo_source'] == 'epel'
+  case node['nginx']['repo_source']
+  when 'epel'
     include_recipe 'yum-epel'
-  elsif node['nginx']['repo_source'] == 'nginx'
+  when 'nginx'
     include_recipe 'nginx::repo'
-  elsif node['nginx']['repo_source'].nil?
+  when nil
     log "node['nginx']['repo_source'] was not set, no additional yum repositories will be installed." do
       level :debug
     end
@@ -33,7 +34,16 @@ if platform_family?('rhel')
     fail ArgumentError, "Unknown value '#{node['nginx']['repo_source']}' was passed to the nginx cookbook."
   end
 elsif platform_family?('debian')
-  include_recipe 'nginx::repo' if node['nginx']['repo_source'] == 'nginx'
+  case node['nginx']['repo_source']
+  when 'nginx', 'phusionpassenger'
+    include_recipe 'nginx::repo'
+  when nil
+    log "node['nginx']['repo_source'] was not set, no additional yum repositories will be installed." do
+      level :debug
+    end
+  else
+    fail ArgumentError, "Unknown value '#{node['nginx']['repo_source']}' was passed to the nginx cookbook."
+  end
 end
 
 package node['nginx']['package_name'] do
