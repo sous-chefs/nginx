8b649d9a8e0103ea1da6025701c341e5a06360e5
Eric Herot <eric.github@herot.com>
2014-02-26 12:02:18 -0500

    Fix rubocop offenses

--- recipes/socketproxy.rb
+++ recipes/socketproxy.rb
@@ -1,4 +1,4 @@
-include_recipe "nginx::commons_dir"
+include_recipe 'nginx::commons_dir'
 
 directory node['nginx']['socketproxy']['root'] do
   owner node['nginx']['socketproxy']['app_owner']
@@ -7,22 +7,20 @@ directory node['nginx']['socketproxy']['root'] do
   action :create
 end
 
-context_names = node['nginx']['socketproxy']['apps'].map{|app,app_conf|
+context_names = node['nginx']['socketproxy']['apps'].map do |app, app_conf|
   app_conf['context_name']
-}
-
-if context_names.uniq.length != context_names.length
-  raise "More than one app has the same context_name configured."
 end
 
-template node['nginx']['dir'] + "/sites-available/socketproxy.conf" do
-  source "modules/socketproxy.conf.erb"
-  owner "root"
-  group "root"
+fail 'More than one app has the same context_name configured.' if context_names.uniq.length != context_names.length
+
+template node['nginx']['dir'] + '/sites-available/socketproxy.conf' do
+  source 'modules/socketproxy.conf.erb'
+  owner 'root'
+  group 'root'
   mode 00644
-  notifies :reload, "service[nginx]"
+  notifies :reload, 'service[nginx]'
 end
 
-link node['nginx']['dir'] + "/sites-enabled/socketproxy.conf" do
-  to node['nginx']['dir'] + "/sites-available/socketproxy.conf"
+link node['nginx']['dir'] + '/sites-enabled/socketproxy.conf' do
+  to node['nginx']['dir'] + '/sites-available/socketproxy.conf'
 end
