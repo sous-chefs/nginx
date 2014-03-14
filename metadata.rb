8b89b6bd05405486e931de475c9b0a0252012c20
Sean OMeara <someara@opscode.com>
2014-03-13 22:19:04 -0400

    updating for devodd

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Opscode, Inc.'
 maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs and configures nginx'
-version           '2.4.4'
+version           '2.4.5'
 
 recipe 'nginx',         'Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available'
 recipe 'nginx::source', 'Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available'
