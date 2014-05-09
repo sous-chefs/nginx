088b3d050d276e77a94958f76ede9a1227b6462b
Michael Saffitz <m@saffitz.com>
2014-05-09 14:20:00 -0700

    [COOK-4644] - add support to nginx::repo for Amazon Linux (which as of 2013.09 is RHEL 6 based)

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -25,6 +25,8 @@ when 'rhel', 'fedora'
   when 'centos'
     # See http://wiki.nginx.org/Install
     default['nginx']['upstream_repository'] = "http://nginx.org/packages/centos/#{node['platform_version'].to_i}/$basearch/"
+  when 'amazon'
+    default['nginx']['upstream_repository'] = "http://nginx.org/packages/rhel/6/$basearch/"
   else
     default['nginx']['upstream_repository'] = "http://nginx.org/packages/rhel/#{node['platform_version'].to_i}/$basearch/"
   end
