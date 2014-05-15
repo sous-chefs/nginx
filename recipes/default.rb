7511a2bc2e10554fc160387b791b25a885eb61f7
Eric Herot <eric.github@herot.com>
2014-05-15 14:33:48 -0400

    [COOK-4601] Add proxy type: Socket
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,3 +25,7 @@ service 'nginx' do
   supports :status => true, :restart => true, :reload => true
   action   :start
 end
+
+node['nginx']['default']['modules'].each do |ngx_module|
+  include_recipe "nginx::#{ngx_module}"
+end
