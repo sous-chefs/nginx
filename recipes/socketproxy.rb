9ebb9d8503d80340f82fcad4d3bb5cea112133de
Eric Herot <eric.github@herot.com>
2014-05-19 14:19:20 -0400

    Improve the handling of unspecified configuration options in socketproxy apps

--- recipes/socketproxy.rb
+++ recipes/socketproxy.rb
@@ -7,11 +7,32 @@ directory node['nginx']['socketproxy']['root'] do
   action :create
 end
 
+if !node['nginx']['socketproxy']['apps'] ||
+  node['nginx']['socketproxy']['apps'] == {}
+  fail 'nginx::socketproxy requires that at least one applicatinon be defined'
+elsif node['nginx']['socketproxy']['apps'].count < 2
+  node.set['nginx']['socketproxy']['default_app'] =
+    node['nginx']['socketproxy']['apps'].keys.first
+else
+  fail "Multiple apps defined but node['nginx']['socketproxy']['default_app']" \
+    ' not defined.' unless node['nginx']['socketproxy']['default_app']
+end
+
 context_names = node['nginx']['socketproxy']['apps'].map do |_app, app_conf|
   app_conf['context_name']
 end
 
 fail 'More than one app has the same context_name configured.' if context_names.uniq.length != context_names.length
+
+node['nginx']['socketproxy']['apps'].each do |app, app_conf|
+  node.set['nginx']['socketproxy']['apps'][app] =
+    node['nginx']['socketproxy']['app_defaults'].merge(
+      app_conf
+    )
+end
+
+Chef::Log.debug "Socketproxy Apps: #{node['nginx']['socketproxy']['apps'].inspect}"
+Chef::Log.debug "Default App: #{node['nginx']['socketproxy']['default_app']}"
 
 template node['nginx']['dir'] + '/sites-available/socketproxy.conf' do
   source 'modules/socketproxy.conf.erb'
