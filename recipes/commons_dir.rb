90ef37eca2613eef0a6611ff5a28d80813435109
Niko Kurtti <niko.kurtti@solita.fi>
2014-06-16 16:30:31 +0300

    delete default config files under conf.d

--- recipes/commons_dir.rb
+++ recipes/commons_dir.rb
@@ -47,3 +47,11 @@ end
     mode  '0755'
   end
 end
+
+if node['nginx']['default_site_enabled'] && (node['platform_family'] == 'rhel' || node['platform_family'] == 'fedora')
+  %w(default.conf example_ssl.conf).each do |config|
+    file "/etc/nginx/conf.d/#{config}" do
+      action :delete
+    end
+  end
+end
