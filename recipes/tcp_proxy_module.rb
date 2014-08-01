401f11b5a000e7959eaec44d69913db1f30d1158
vpryimak <viacheslv.pr@randrmusic.com>
2014-08-01 17:32:05 +0300

    build module without patch

--- recipes/tcp_proxy_module.rb
+++ recipes/tcp_proxy_module.rb
@@ -3,6 +3,7 @@ tcp_src_filename = ::File.basename(node['nginx']['tcp_proxy_module']['url'])
 tcp_src_filepath = "#{Chef::Config['file_cache_path']}/#{tcp_src_filename}"
 tcp_extract_path = "#{Chef::Config['file_cache_path']}/tcp_proxy_module"
 nginx_src_filepath = "#{Chef::Config['file_cache_path']}/nginx-#{node['nginx']['source']['version']}"
+
 remote_file tcp_src_filepath do
   source   node['nginx']['tcp_proxy_module']['url']
   owner    'root'
@@ -16,11 +17,11 @@ bash 'extract_tcp_proxy_module' do
     mkdir -p #{tcp_extract_path}
     unzip #{tcp_src_filename} -d #{tcp_extract_path}
     mv #{tcp_extract_path}/*/* #{tcp_extract_path}
-    cd #{nginx_src_filepath}
-    patch -p1 < #{tcp_extract_path}/tcp.patch
+    #cd #{nginx_src_filepath}
+    #patch -p1 < #{tcp_extract_path}/tcp.patch
   EOH
   not_if { ::File.exists?(tcp_extract_path) }
 end
 
 node.run_state['nginx_configure_flags'] =
-  node.run_state['nginx_configure_flags'] | ["--add-module=#{tcp_extract_path}"]
+node.run_state['nginx_configure_flags'] | ["--add-module=#{tcp_extract_path}"]
