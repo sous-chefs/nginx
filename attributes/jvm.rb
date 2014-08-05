5edf1456d0eb72c83d67bf2eb30dbfd8a9d061e5
vpryimak <viacheslv.pr@randrmusic.com>
2014-08-05 15:41:04 +0300

    add new module

---
+++ attributes/jvm.rb
@@ -0,0 +1,1 @@
+default['nginx']['jvm_module']['url'] = 'https://github.com/nulab/nginx-upstream-jvm-route/archive/master.zip'
---
+++ recipes/jvm_module.rb
@@ -0,0 +1,26 @@
+jvm_src_filename = ::File.basename(node['nginx']['jvm']['url'])
+jvm_src_filepath = "#{Chef::Config['file_cache_path']}/#{jvm_src_filename}"
+jvm_extract_path = "#{Chef::Config['file_cache_path']}/jvm_module"
+nginx_src_filepath = "#{Chef::Config['file_cache_path']}/nginx-#{node['nginx']['source']['version']}"
+
+remote_file tcp_src_filepath do
+  source   node['nginx']['jvm_module']['url']
+  owner    'root'
+  group    node['root_group']
+  mode     '0644'
+end
+
+bash 'extract_jvm_module' do
+  cwd  ::File.dirname(jvm_src_filepath)
+  code <<-EOH
+    mkdir -p #{jvm_extract_path}
+    unzip #{tcp_src_filename} -d #{jvm_extract_path}
+    mv #{jvm_extract_path}/*/* #{jvm_extract_path}
+    cd #{nginx_src_filepath}
+    patch -p0 < #{jvm_extract_path}/jvm_route.patch
+  EOH
+  not_if { ::File.exists?(jvm_extract_path) }
+end
+
+node.run_state['nginx_configure_flags'] =
+node.run_state['nginx_configure_flags'] | ["--add-module=#{jvm_extract_path}"]
