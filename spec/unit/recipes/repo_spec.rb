b6a8c2e3347047395d820063e0d7efdfecf80a61
Julien Berard <jujugrrr@gmail.com>
2014-09-02 00:00:34 +0200

    Added specs for repos
    Signed-off-by: Mike Fiedler <miketheman@gmail.com>

---
+++ spec/unit/recipes/repo_spec.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+describe 'nginx::repo' do
+  context 'Debian' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(:platform => 'debian', :version  => '7.0').converge(described_recipe)
+    end
+
+    it 'includes apt recipe' do
+      expect(chef_run).to include_recipe('apt::default')
+    end
+
+    it 'adds apt repository' do
+      expect(chef_run).to add_apt_repository('nginx')
+    end
+  end
+
+  context 'Rhel' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(:platform => 'centos', :version  => '6.5').converge(described_recipe)
+    end
+
+    it 'adds yum repository' do
+      expect(chef_run).to create_yum_repository('nginx')
+    end
+  end
+
+  context 'Fedora' do
+    let(:chef_run) do
+      ChefSpec::Runner.new(:platform => 'fedora', :version  => '20').converge(described_recipe)
+    end
+
+    it 'adds yum repository' do
+      expect(chef_run).to create_yum_repository('nginx')
+    end
+  end
+end
