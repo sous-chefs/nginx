75d17ceac3b84aaa5f7549a0b7c82a0078e5a11b
Mike Fiedler <miketheman@gmail.com>
2014-09-02 08:52:35 -0400

    prevent resource cloning wanrings during testing
    This is a known issue, since many recipes redeclare the `service` resource
    during compilation, instead of doing it one place and controlling it with
    notifications and attributes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
+# encoding: utf-8
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  # prevent any WARN messages during testing
+  config.log_level = :error
+end
 
 at_exit { ChefSpec::Coverage.report! }
