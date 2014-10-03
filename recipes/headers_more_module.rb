2548a6953977b890f25bb82c3a6aa7507d10a85e
Josh Padnick <josh@omedix.com>
2014-10-03 09:47:21 -0700

    Apparnently File.exist is preferred over File.exists
    See https://travis-ci.org/miketheman/nginx/jobs/36982759

--- recipes/headers_more_module.rb
+++ recipes/headers_more_module.rb
@@ -45,7 +45,7 @@ bash 'extract_headers_more' do
     mv -f #{module_location}/agentz*/* #{module_location}
     rm -rf #{module_location}/agentz*
   EOH
-  not_if { ::File.exists?("#{module_location}/headers-more-nginx-module-#{node['nginx']['headers_more']['version']}/config") }
+  not_if { ::File.exist?("#{module_location}/headers-more-nginx-module-#{node['nginx']['headers_more']['version']}/config") }
 end
 
 node.run_state['nginx_configure_flags'] =
