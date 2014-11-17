4060f3efd3a5232e34e5e23058ee7d7011a3a2c6
Mike Fiedler <miketheman@gmail.com>
2014-11-16 21:54:25 -0500

    don't skip all spec tests when using filters
    With the addition of default rspec tags, all tests were being filtered out
    instead of being run. This should reinstate the tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'chefspec/berkshelf'
 RSpec.configure do |config|
   # prevent any WARN messages during testing
   config.log_level = :error
+
+  # run all specs when using a filter, but no spec match
+  config.run_all_when_everything_filtered = true
 end
 
 ChefSpec::Coverage.start!
