e6b7f76b30a7b372c62390e9fd857cf619e48c65
Mike Fiedler <miketheman@gmail.com>
2014-11-19 23:20:33 -0500

    add tests for geoip module

---
+++ spec/unit/recipes/http_geoip_module_spec.rb
@@ -0,0 +1,36 @@
+# Encoding: utf-8
+require 'spec_helper'
+
+describe 'nginx::http_geoip_module', :focus do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new do |node|
+      node.set['nginx']['source']['modules'] = ['nginx::http_geoip_module']
+    end.converge(described_recipe)
+  end
+
+  it 'installs libtool package' do
+    expect(chef_run).to install_package('libtool')
+  end
+
+  # TODO: additional resources are cloned during ChefSpec runs,
+  # leading to inaccurate counts.
+  it 'retrieves remote files to cache' do
+    expect(chef_run).to create_remote_file("#{Chef::Config['file_cache_path']}/GeoIP-1.4.8.tar.gz")
+    expect(chef_run).to create_remote_file("#{Chef::Config['file_cache_path']}/GeoIP.dat.gz")
+    expect(chef_run).to create_remote_file("#{Chef::Config['file_cache_path']}/GeoLiteCity.dat.gz")
+  end
+
+  it 'creates lib directory' do
+    expect(chef_run).to create_directory('/srv/geoip')
+  end
+
+  it 'expands the retrieved files' do
+    expect(chef_run).to run_bash('extract_geolib')
+    expect(chef_run).to run_bash('gunzip_geo_lite_country_dat')
+    expect(chef_run).to run_bash('gunzip_geo_lite_city_dat')
+  end
+
+  it 'creates config file' do
+    expect(chef_run).to create_template('/etc/nginx/conf.d/http_geoip.conf')
+  end
+end
---
+++ test/integration/modules/serverspec/http_geoip_module_spec.rb
@@ -0,0 +1,17 @@
+# Encoding: utf-8
+require_relative 'spec_helper'
+
+describe file('/etc/nginx/conf.d/http_geoip.conf') do
+  it { should be_file }
+
+  its(:content) { should match(%r{/srv/geoip/GeoIP.dat}) }
+end
+
+# FIXME: This will not sustain a version update
+describe command('/opt/nginx-1.4.4/sbin/nginx -V') do
+  its(:stdout) { should match(/--with-http_geoip_module/) }
+end
+
+describe service('nginx') do
+  it { should be_running }
+end
