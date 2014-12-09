4826faa120bbf7fa3424fe15727f38904ff13d19
Thomas Berry <thom.berry@gmail.com>
2014-12-09 14:23:49 -0500

    removed checksum, adding bsw_gpg cookbook

--- Berksfile
+++ Berksfile
@@ -1,6 +1,8 @@
 source 'https://supermarket.getchef.com'
 metadata
 
+cookbook 'bsw_gpg'
+
 group :integration do
   cookbook 'apt', '~> 2.0'
 end
--- recipes/source.rb
+++ recipes/source.rb
@@ -55,9 +55,11 @@ packages.each do |name|
   package name
 end
 
+
+
+# remove checksum
 remote_file nginx_url do
   source   nginx_url
-  checksum node['nginx']['source']['checksum']
   path     src_filepath
   backup   false
 end
