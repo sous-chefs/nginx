981058588835849b85f5a299498b93ac1b52bcf3
Mike Fiedler <miketheman@gmail.com>
2015-01-31 22:58:52 -0500

    add metadata.rb and Berksfile

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name              'nginx'
+maintainer        'Mike Fiedler'
+maintainer_email  'miketheman@gmail.com'
+license           'Apache 2.0'
+description       'Installs and configures nginx'
+version           '2.99.0'
