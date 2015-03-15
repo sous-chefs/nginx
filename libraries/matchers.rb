80fe8c940e32831dbc00d90318b2a40ab3408d1d
Mike Fiedler <miketheman@gmail.com>
2015-03-15 10:21:02 -0400

    rename matchers file and warp in if to prevent load

---
+++ libraries/matchers.rb
@@ -0,0 +1,20 @@
+# Custom ChefSpec matchers
+if defined?(ChefSpec)
+  module ChefSpec::Matchers
+    RSpec::Matchers.define :enable_nginx_site do |site|
+      match do |chef_run|
+        chef_run.resource_collection.all_resources.any? do |resource|
+          resource.resource_name == :execute && resource.name =~ /.*nxensite.*#{site}/
+        end
+      end
+    end
+
+    RSpec::Matchers.define :disable_nginx_site do |site|
+      match do |chef_run|
+        chef_run.resource_collection.all_resources.any? do |resource|
+          resource.resource_name == :execute && resource.name =~ /.*nxdissite.*#{site}/
+        end
+      end
+    end
+  end
+end
