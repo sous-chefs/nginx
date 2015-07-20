32d853cf243ad561aa4c296820f68ba576400a85
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-07-20 19:23:15 +0300

    Add patch for jvm route from ajp_module

--- attributes/ajp_module.rb
+++ attributes/ajp_module.rb
@@ -1,2 +1,3 @@
 default['nginx']['ajp_module']['version'] = 'v0.3.0'
 default['nginx']['ajp_module']['url'] = 'https://github.com/yaoweibin/nginx_ajp_module/archive/{version}.zip'
+default['nginx']['ajp_module']['patch_jvm_route'] = false
--- recipes/ajp_module.rb
+++ recipes/ajp_module.rb
@@ -4,6 +4,9 @@ ajp_src_name = "ajp_module_#{ajp_version}"
 ajp_archive = "#{ajp_src_name}#{::File.extname(ajp_url)}"
 ajp_archive_path = ::File.join(Chef::Config['file_cache_path'], ajp_archive)
 ajp_src_path = ::File.join(Chef::Config['file_cache_path'], ajp_src_name)
+ajp_patch_path = ::File.join(ajp_src_path, 'ajp.patch')
+
+nginx_src_path = ::File.join(Chef::Config['file_cache_path'], "/nginx-#{node['nginx']['source']['version']}")
 
 module_downloader = remote_file ajp_archive_path do
   source ajp_url
@@ -22,6 +25,24 @@ bash 'extract_ajp_module' do
   subscribes :run, module_downloader, :immediately
   only_if "test -f #{ajp_archive_path}"
   not_if "test -d #{ajp_src_path}"
+end
+
+execute 'apply_ajp_patch' do
+  cwd  nginx_src_path
+  command "patch -p1 < #{ajp_patch_path}"
+  subscribes :run, 'bash[extract_ajp_module]', :immediately
+  not_if "patch -p1 --dry-run --reverse --silent < #{ajp_patch_path}"
+  only_if "test -f #{ajp_patch_path}"
+end
+
+if node.run_state['upstream_check_module_path'] && node['nginx']['ajp_module']['patch_jvm_route']
+  patch_file_path = ::File.join(node.run_state['upstream_check_module_path'], 'ngx_http_upstream_jvm_route_module.patch')
+  execute 'apply_jvm_route_upstream_check_patch' do
+    cwd ajp_src_path
+    command "patch -p1 #{patch_file_path}"
+    not_if "patch -p1 --dry-run --reverse --silent #{patch_file_path}"
+    only_if "test -f #{patch_file_path}"
+  end
 end
 
 node.run_state['nginx_configure_flags'] =
