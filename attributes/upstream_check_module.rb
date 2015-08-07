0d19aa712d69f5e0f2fed0c3e10968651636b93a
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-08-07 16:07:54 +0300

    Add nginx 1.7.5 support Set last version from master branch (support nginx 1.9.2)

--- attributes/upstream_check_module.rb
+++ attributes/upstream_check_module.rb
@@ -1,4 +1,4 @@
-default['nginx']['upstream_check_module']['version'] = 'v0.3.0'
+default['nginx']['upstream_check_module']['version'] = '10782eaff51872a8f44e65eed89bbe286004bcb1'
 default['nginx']['upstream_check_module']['url'] = 'https://github.com/yaoweibin/nginx_upstream_check_module/archive/{version}.zip'
 default['nginx']['upstream_check_module']['patches'] = {
   Gem::Version.new('0.0.0') => 'check.patch',
@@ -10,5 +10,6 @@ default['nginx']['upstream_check_module']['patches'] = {
   Gem::Version.new('1.3.9') => 'check_1.2.6+.patch',
   Gem::Version.new('1.5.12') => 'check_1.5.12+.patch',
   Gem::Version.new('1.7.2') => 'check_1.7.2+.patch',
+  Gem::Version.new('1.7.5') => 'check_1.7.5+.patch',
   Gem::Version.new('1.9.2') => 'check_1.9.2+.patch'
 }
