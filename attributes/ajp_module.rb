75ea895017e6327d53a6aa9a813455a6ee3b1b51
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-08-07 13:44:09 +0300

    Bump module version

--- attributes/ajp_module.rb
+++ attributes/ajp_module.rb
@@ -1,3 +1,3 @@
-default['nginx']['ajp_module']['version'] = 'v0.3.0'
+default['nginx']['ajp_module']['version'] = 'bf6cd93f2098b59260de8d494f0f4b1f11a84627'
 default['nginx']['ajp_module']['url'] = 'https://github.com/yaoweibin/nginx_ajp_module/archive/{version}.zip'
 default['nginx']['ajp_module']['patch_jvm_route'] = false
