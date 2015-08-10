7c8cdef5764a856a5ea06c045a4bb7f638fd40fe
Mike Fiedler <miketheman@gmail.com>
2015-08-10 08:35:43 -0400

    Update specs for upstart shared examples
    In order to correctly trigger the population of the service providers
    array, we have to mock out the expected call, as the resolver uses
    on-disk files to determine the results.
    
    See https://github.com/chef/chef/blob/master/lib/chef/platform/service_helpers.rb

--- spec/libraries_specs/resource/nginx_service/create/debian_7_spec.rb
+++ spec/libraries_specs/resource/nginx_service/create/debian_7_spec.rb
@@ -1,4 +1,8 @@
 describe 'resource_nginx_service :create on debian 7' do
+  before do
+    allow(Chef::Platform::ServiceHelpers).to receive(:service_resource_providers).and_return([:upstart])
+  end
+
   cached(:chef_run) do
     ChefSpec::SoloRunner.new(
       step_into: 'nginx_service',
@@ -11,6 +15,7 @@ describe 'resource_nginx_service :create on debian 7' do
 
   it_behaves_like 'nginx_service :create', 'example'
   it_behaves_like 'nginx_service :start', 'example'
+  it_behaves_like 'nginx_service #upstart', 'example'
 
   it_behaves_like 'nginx_config :create', 'example'
 end
