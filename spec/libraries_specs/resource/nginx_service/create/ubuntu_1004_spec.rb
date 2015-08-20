85ff11e96df6e866dd6629f98bf21d10317b299b
Mike Fiedler <miketheman@gmail.com>
2015-08-20 07:31:59 -0400

    Add a spec test for older Ubuntu w/sysvinit
    This is not a guarantee that Ubuntu 10.04 works, or that it uses
    sysvinit exclusively, rather that if there is a system that reports a
    `debian` service provider, it will be used as a `sysvinit` one.

---
+++ spec/libraries_specs/resource/nginx_service/create/ubuntu_1004_spec.rb
@@ -0,0 +1,21 @@
+describe 'resource_nginx_service :create on ubuntu 10.04' do
+  before do
+    allow(Chef::Platform::ServiceHelpers).to receive(:service_resource_providers).and_return([:debian])
+  end
+
+  cached(:chef_run) do
+    ChefSpec::SoloRunner.new(
+      step_into: 'nginx_service',
+      platform: 'ubuntu',
+      version: '10.04'
+    ).converge('nginx::example')
+  end
+
+  it_behaves_like 'create a named nginx_service', 'example'
+
+  it_behaves_like 'nginx_service :create', 'example'
+  it_behaves_like 'nginx_service :start', 'example'
+  it_behaves_like 'nginx_service #sysvinit', 'example'
+
+  it_behaves_like 'nginx_config :create', 'example'
+end
