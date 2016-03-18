02e9c09475c28d05c7f0104615ffef3d60d2544c
Miguel Ferreira <miguelferreira@me.com>
2016-03-18 07:13:17 +0100

    Remove cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,3 @@ version           '2.99.0'
 
 issues_url 'https://github.com/miketheman/nginx/issues'
 source_url 'https://github.com/miketheman/nginx'
-
-depends 'apt',      '~> 2.6.1'
-depends 'yum',      '~> 3.5.2'
-depends 'yum-epel', '~> 0.6.3'
