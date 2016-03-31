d7f5e86be12b0d3e12f33a6e6471a1a861dbcd51
Jesse Cotton <JCotton1123@gmail.com>
2016-03-31 12:35:08 -0700

    Implement ohai 7 plugin and logic to select correct plugin ver

--- spec/unit/recipes/ohai_plugin_spec.rb
+++ spec/unit/recipes/ohai_plugin_spec.rb
@@ -10,6 +10,32 @@ describe 'nginx::ohai_plugin' do
     pending('ohai cookbook does not provide a matcher or ohai resource')
   end
 
+  context 'ohai 8' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new do |node|
+        node.automatic['chef_packages']['ohai']['version'] = '8.1.4'
+      end.converge(described_recipe)
+    end
+    it 'select nginx ohai7 configuration' do
+      expect(chef_run).to create_template('/etc/chef/ohai_plugins/nginx.rb').with(
+        source: 'plugins/ohai7-nginx.rb.erb'
+      )
+    end
+  end
+
+  context 'ohai 6' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new do |node|
+        node.automatic['chef_packages']['ohai']['version'] = '6.1.3'
+      end.converge(described_recipe)
+    end
+    it 'select nginx ohai7 configuration' do
+      expect(chef_run).to create_template('/etc/chef/ohai_plugins/nginx.rb').with(
+        source: 'plugins/ohai6-nginx.rb.erb'
+      )
+    end
+  end
+
   it 'create nginx ohai configuration' do
     expect(chef_run).to render_file('/etc/chef/ohai_plugins/nginx.rb')
   end
