78bad376382a023d93fd6d90b650992aebb45672
Tim Smith <tsmith84@gmail.com>
2016-04-06 16:13:24 -0700

    Remove specs that don't apply now

--- spec/recipes_specs/nginx_service_test/default_spec.rb
+++ spec/recipes_specs/nginx_service_test/default_spec.rb
@@ -10,15 +10,3 @@ describe 'nginx_service_test::multi' do
   it_behaves_like 'create a named nginx_service', 'multi1'
   it_behaves_like 'create a named nginx_service', 'multi2'
 end
-
-describe 'nginx_service_test::nginx_repo' do
-  cached(:chef_run) do
-    ChefSpec::SoloRunner.new(
-      platform: 'debian', version: '7.0'
-    ).converge(described_recipe)
-  end
-
-  it 'adds the nginx.org apt repo' do
-    expect(chef_run).to add_apt_repository('nginx.org')
-  end
-end
