05ff17c08172d853c4cbd582c8749d62aee679a7
Joe Doss <jdoss@kennasecurity.com>
2016-06-20 16:50:55 -0500

    Bump build-essential too.

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ recipe 'nginx::source', 'Installs nginx from source and sets up configuration wi
 
 depends 'apt',             '~> 2.9'
 depends 'bluepill',        '~> 2.3'
-depends 'build-essential', '~> 2.0'
+depends 'build-essential', '~> 3.0'
 depends 'ohai',            '~> 3.0'
 depends 'runit',           '~> 1.2'
 depends 'yum-epel',        '~> 0.3'
