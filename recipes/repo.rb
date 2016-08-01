07bfbc19f08c626214c6997c07e525431f93958c
Tim Smith <tsmith@chef.io>
2016-08-01 15:01:02 -0700

    Fix misaligned resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -22,10 +22,10 @@ case node['platform_family']
 when 'rhel', 'fedora'
 
   yum_repository 'nginx' do
-    description 'Nginx.org Repository'
-    baseurl         node['nginx']['upstream_repository']
-    gpgkey      'http://nginx.org/keys/nginx_signing.key'
-    action :create
+    description  'Nginx.org Repository'
+    baseurl      node['nginx']['upstream_repository']
+    gpgkey       'http://nginx.org/keys/nginx_signing.key'
+    action       :create
   end
 
 when 'debian'
