46c327573e7fa05711e871fcdfec7002ef62f15b
Tim Smith <tsmith@chef.io>
2016-08-01 14:15:46 -0700

    Use the latest page speed module
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/pagespeed.rb
+++ attributes/pagespeed.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: nginx
 # Recipe:: pagespeed_module
 #
-default['nginx']['pagespeed']['version'] = '1.8.31.4'
+default['nginx']['pagespeed']['version'] = '1.11.33.2'
 default['nginx']['pagespeed']['url']     = "https://github.com/pagespeed/ngx_pagespeed/archive/release-#{node['nginx']['pagespeed']['version']}-beta.tar.gz"
 default['nginx']['psol']['url']          = "https://dl.google.com/dl/page-speed/psol/#{node['nginx']['pagespeed']['version']}.tar.gz"
 default['nginx']['pagespeed']['packages']['rhel'] = %w(gcc-c++ pcre-dev pcre-devel zlib-devel make)
