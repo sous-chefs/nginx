7658bc1de4a16463f3105b14596c1f79489d9524
Tim Smith <tsmith@chef.io>
2016-08-12 15:49:06 -0700

    Require Chef 12+
    Remove the need to maintain multiple Ohai templates
    Allows us to use the ohai_plugin custom resource in Ohai 4.1+ cookbook, which works much better
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- README.md
+++ README.md
@@ -6,13 +6,11 @@ Installs nginx from package OR source code and sets up configuration handling si
 
 ## nginx vs. chef_nginx
 
-This cookbook is a fork from the 2.7.x branch of the [community nginx cookbook](https://github.com/miketheman/nginx).  This branch
-will be actively supported by Chef Software.
+This cookbook is a fork from the 2.7.x branch of the [community nginx cookbook](https://github.com/miketheman/nginx). This branch will be actively supported by Chef Software.
 
 We will continue to migrate the cookbook to a more resource driven model, with incremental changes that are non-breaking when possible.
 
-If this cookbook is eventually merged into the `nginx` namespace on the supermarket, this `chef_nginx` cookbook will receive updates
-in parallel (mirroring) in order to minimize disruption to users that have adopted this cookbook.
+If this cookbook is eventually merged into the `nginx` namespace on the supermarket, this `chef_nginx` cookbook will receive updates in parallel (mirroring) in order to minimize disruption to users that have adopted this cookbook.
 
 ## Requirements
 
@@ -42,7 +40,7 @@ Other Debian and RHEL family distributions are assumed to work.
 
 ### Chef
 
-- Chef 11+
+- Chef 12+
 
 ## Attributes
 
--- metadata.rb
+++ metadata.rb
@@ -1,18 +1,18 @@
-name              'chef_nginx'
-maintainer        'Chef Software, Inc.'
-maintainer_email  'cookbooks@chef.io'
-license           'Apache 2.0'
-description       'Installs and configures nginx'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name 'chef_nginx'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs and configures nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.9.0'
 
-recipe 'chef_nginx',         'Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available'
+recipe 'chef_nginx', 'Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available'
 recipe 'chef_nginx::source', 'Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available'
 
 depends 'apt'
 depends 'bluepill'
 depends 'build-essential'
-depends 'ohai', '< 4.0'
+depends 'ohai', '>= 4.1.0'
 depends 'runit', '>= 1.6.0'
 depends 'yum'
 depends 'yum-epel'
@@ -28,7 +28,7 @@ supports 'scientific'
 supports 'ubuntu'
 supports 'suse'
 
-source_url 'https://github.com/chef-cookbooks/nginx' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/nginx/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/nginx'
+issues_url 'https://github.com/chef-cookbooks/nginx/issues'
 
-chef_version '>= 11' if respond_to?(:chef_version)
+chef_version '>= 12' if respond_to?(:chef_version)
