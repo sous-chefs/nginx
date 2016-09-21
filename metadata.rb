60cd1acc47f606a5bce6d32a4dee6824c9eb12cf
Tim Smith <tsmith@chef.io>
2016-09-21 13:35:21 -0700

    Require compat_resource with notification fixes

--- metadata.rb
+++ metadata.rb
@@ -13,7 +13,7 @@ depends 'build-essential'
 depends 'ohai', '>= 4.1.0'
 depends 'yum-epel'
 depends 'runit', '>= 1.6.0'
-depends 'compat_resource', '>= 12.14.3'
+depends 'compat_resource', '>= 12.14.6'
 depends 'zypper'
 
 supports 'amazon'
