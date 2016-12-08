cc6d8c7b922cf61d6f65330f903023e9461ffe5f
Tim Smith <tsmith@chef.io>
2016-12-07 23:05:28 -0800

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,10 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-
   config.color = true               # Use color in STDOUT
   config.formatter = :documentation # Use the specified formatter
   config.log_level = :error         # Avoid deprecation notice SPAM
-  
+
   # run all specs when using a filter, but no spec match
   config.run_all_when_everything_filtered = true
 
