bf0f67983d23de006b4151882c9734db16177c40
Tim Smith <tsmith@chef.io>
2017-02-12 06:21:45 -0800

    Resolve a Chef 13 deprecation warning
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/commons_conf.rb
+++ recipes/commons_conf.rb
@@ -32,6 +32,7 @@ template "#{node['nginx']['dir']}/sites-available/default" do
   notifies :reload, 'service[nginx]', :delayed
 end
 
-nginx_site 'default' do
+nginx_site 'Disable default site' do
+  name 'default'
   action node['nginx']['default_site_enabled'] ? :enable : :disable
 end
