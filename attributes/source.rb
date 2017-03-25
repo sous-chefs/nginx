35cd607a42074d3a43f978552e855ea9469fadf3
Tim Smith <tsmith@chef.io>
2017-03-24 22:07:02 -0700

    Install 1.10.3 on source installs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/source.rb
+++ attributes/source.rb
@@ -41,7 +41,7 @@ default['nginx']['source']['default_configure_flags'] = %W(
 default['nginx']['configure_flags']    = []
 default['nginx']['source']['version']  = node['nginx']['version']
 default['nginx']['source']['url']      = "http://nginx.org/download/nginx-#{node['nginx']['source']['version']}.tar.gz"
-default['nginx']['source']['checksum'] = '1fd35846566485e03c0e318989561c135c598323ff349c503a6c14826487a801'
+default['nginx']['source']['checksum'] = '75020f1364cac459cb733c4e1caed2d00376e40ea05588fb8793076a4c69dd90'
 default['nginx']['source']['modules']  = %w(
   chef_nginx::http_ssl_module
   chef_nginx::http_gzip_static_module
