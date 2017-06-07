fefca161f3340eb21b639b2fcdca74e93f6e09f3
Ian C. Ward <ian.ward@disney.com>
2017-06-07 16:03:45 -0400

    Fixed #95: make repo properties configurable
    - Move repo options (source, signing/gpg key) to attributes
    - Simplify conditional logic for default['nginx']['upstream_repository']
    - Use the same signing key for all repo types
    - Alphabetize attribute filenames in README
    - Add documentation for repo attributes to README
    
    Signed-off-by: Ian C. Ward <ian.ward@disney.com>

--- spec/unit/recipes/repo_spec.rb
+++ spec/unit/recipes/repo_spec.rb
@@ -3,37 +3,54 @@ require 'spec_helper'
 describe 'chef_nginx::repo' do
   context 'Ubuntu' do
     let(:chef_run) do
-      ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04').converge(described_recipe)
+      ChefSpec::ServerRunner.new(
+        platform: 'ubuntu', version: '16.04'
+      ).converge(described_recipe)
     end
 
     it 'adds apt repository' do
-      expect(chef_run).to add_apt_repository('nginx')
+      expect(chef_run).to add_apt_repository('nginx').with(
+        uri: 'http://nginx.org/packages/ubuntu',
+        key: 'http://nginx.org/keys/nginx_signing.key'
+      )
     end
   end
 
   context 'Debian' do
     let(:chef_run) do
-      ChefSpec::ServerRunner.new(platform: 'debian', version: '8.6').converge(described_recipe)
+      ChefSpec::ServerRunner.new(
+        platform: 'debian', version: '8.6'
+      ).converge(described_recipe)
     end
 
     it 'adds apt repository' do
-      expect(chef_run).to add_apt_repository('nginx')
+      expect(chef_run).to add_apt_repository('nginx').with(
+        uri: 'http://nginx.org/packages/debian',
+        key: 'http://nginx.org/keys/nginx_signing.key'
+      )
     end
   end
 
   context 'SUSE' do
     let(:chef_run) do
-      ChefSpec::ServerRunner.new(platform: 'opensuse', version: '13.2').converge(described_recipe)
+      ChefSpec::ServerRunner.new(
+        platform: 'opensuse', version: '13.2'
+      ).converge(described_recipe)
     end
 
     it 'adds zypper repository' do
-      expect(chef_run).to add_zypper_repo('nginx')
+      expect(chef_run).to add_zypper_repo('nginx').with(
+        uri: 'http://nginx.org/packages/sles/12',
+        key: 'http://nginx.org/keys/nginx_signing.key'
+      )
     end
   end
 
   context 'RHEL' do
     let(:chef_run) do
-      ChefSpec::ServerRunner.new(platform: 'centos', version: '6.8').converge(described_recipe)
+      ChefSpec::ServerRunner.new(
+        platform: 'centos', version: '6.8'
+      ).converge(described_recipe)
     end
 
     it 'adds yum repository' do
@@ -43,7 +60,9 @@ describe 'chef_nginx::repo' do
 
   context 'Fedora' do
     let(:chef_run) do
-      ChefSpec::ServerRunner.new(platform: 'fedora', version: '24').converge(described_recipe)
+      ChefSpec::ServerRunner.new(
+        platform: 'fedora', version: '24'
+      ).converge(described_recipe)
     end
 
     it "logs a message that the repo doesn't support Fedora" do
