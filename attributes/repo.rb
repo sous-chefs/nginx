3e894fba981034e4e7afca2735bdee001438f9d0
Tim Smith <tsmith@chef.io>
2017-06-07 23:39:32 -0700

    Cookstyle fix Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -22,15 +22,15 @@
 default['nginx']['upstream_repository'] =
   case node['platform_family']
   when 'rhel', 'fedora'
-      case node['platform']
-      when 'centos'
-        # See http://wiki.nginx.org/Install
-        "http://nginx.org/packages/centos/#{node['platform_version'].to_i}/$basearch/"
-      when 'amazon'
-        'http://nginx.org/packages/rhel/6/$basearch/'
-      else
-        "http://nginx.org/packages/rhel/#{node['platform_version'].to_i}/$basearch/"
-      end
+    case node['platform']
+    when 'centos'
+      # See http://wiki.nginx.org/Install
+      "http://nginx.org/packages/centos/#{node['platform_version'].to_i}/$basearch/"
+    when 'amazon'
+      'http://nginx.org/packages/rhel/6/$basearch/'
+    else
+      "http://nginx.org/packages/rhel/#{node['platform_version'].to_i}/$basearch/"
+    end
   when 'debian'
     "http://nginx.org/packages/#{node['platform']}"
   when 'suse'
