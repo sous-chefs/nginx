f63f495aa863a792e968ab6e45f588d24708584e
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-09-12 15:57:09 +0300

    Update versions and checksums for lua-nginx-module and echo-nginx-module
    Signed-off-by: Ilya Dolgirev <ilya.dolgirev@gmail.com>

--- attributes/echo.rb
+++ attributes/echo.rb
@@ -19,6 +19,6 @@
 # limitations under the License.
 #
 
-default['nginx']['echo']['version']        = '0.59'
+default['nginx']['echo']['version']        = '0.61'
 default['nginx']['echo']['url']            = "https://github.com/openresty/echo-nginx-module/archive/v#{node['nginx']['echo']['version']}.tar.gz"
-default['nginx']['echo']['checksum']       = '9b319ad7836202883128d2b9c24ed818082541df57ef7f2065b7557085c603cd'
+default['nginx']['echo']['checksum']       = '2e6a03032555f5da1bdff2ae96c96486f447da3da37c117e0f964ae0753d22aa'
