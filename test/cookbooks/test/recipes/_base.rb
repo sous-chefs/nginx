458b6f69f047bb03a9b2f76f1be5fc446cfbc73b
Tim Smith <tsmith@chef.io>
2017-09-12 10:30:05 -0700

    Avoid apt_update on suse
    We need to fix this in core chef as well
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/_base.rb
+++ test/cookbooks/test/recipes/_base.rb
@@ -1,4 +1,4 @@
-apt_update 'update'
+apt_update 'update' if platform_family?('debian')
 
 # needed for the specs
 package 'curl'
--- test/cookbooks/test/recipes/upstream_repo.rb
+++ test/cookbooks/test/recipes/upstream_repo.rb
@@ -1,4 +1,2 @@
-apt_update 'update'
-
 include_recipe 'test::_base'
 include_recipe 'test::_test_site'
