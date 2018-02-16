c38df0657bd280b5ca0f16f95129dfb45e55617f
Tim Smith <tsmith@chef.io>
2018-02-16 10:46:55 -0800

    Remove compat_resource and require Chef 12.7+
    12.7 came out a LONG time ago at this point. Compat_resource is
    fundamentally flawed and causes lots of issues.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Other Debian and RHEL family distributions are assumed to work.
 
 ### Chef
 
-- Chef 12.1+
+- Chef 12.7+
 
 ## Attributes
 
--- metadata.rb
+++ metadata.rb
@@ -12,7 +12,6 @@ recipe 'nginx::source', 'Installs nginx from source and sets up configuration wi
 depends 'build-essential'
 depends 'ohai', '>= 4.1.0'
 depends 'yum-epel'
-depends 'compat_resource', '>= 12.16.3'
 depends 'zypper'
 
 supports 'amazon'
@@ -29,4 +28,4 @@ supports 'opensuseleap'
 
 source_url 'https://github.com/chef-cookbooks/nginx'
 issues_url 'https://github.com/chef-cookbooks/nginx/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.7' if respond_to?(:chef_version)
