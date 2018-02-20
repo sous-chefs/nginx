7ffa50961ae7c960e985cbdd210ce99ae105d090
Alexander Belyaev <alexbel@lavabit.com>
2018-02-20 11:34:45 -0800

    Add map_hash_max_size as configuration option
    Signed-off-by: Alexander Belyaev <alexbel@lavabit.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -120,6 +120,7 @@ default['nginx']['proxy_read_timeout']          = nil
 default['nginx']['client_body_buffer_size']     = nil
 default['nginx']['client_max_body_size']        = nil
 default['nginx']['large_client_header_buffers'] = nil
+default['nginx']['map_hash_max_size']           = nil
 default['nginx']['default']['modules']          = []
 
 default['nginx']['extra_configs'] = {}
