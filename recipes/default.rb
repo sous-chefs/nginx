f0bf28406f262e91f40764fe0868a83df875b567
Arun Sanna <sanna.arunchowdary@gmail.com>
2018-05-02 19:22:08 -0400

    updated flag to control service installtion

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,6 +23,6 @@ nginx_cleanup_runit 'cleanup' if node['nginx']['cleanup_runit']
 
 include_recipe "nginx::#{node['nginx']['install_method']}"
 
-node['nginx']['default']['modules'].each do |ngx_module|
-  include_recipe "nginx::#{ngx_module}"
-end
+#node['nginx']['default']['modules'].each do |ngx_module|
+#  include_recipe "nginx::#{ngx_module}"
+#end
