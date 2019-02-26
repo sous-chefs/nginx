1d42f9949d063d261f30690fd510cc3e4b3bc44e
Dan Webb <dan.webb@damacus.io>
2019-02-26 08:04:14 +0000

    Make the helpers really ugly until we can simplify that logic

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -21,18 +21,16 @@
 
 module Nginx
   module Cookbook
-      module Helpers
-        # pidfile is hard to determine on Debian systems.
-        # Upstream packages and older distro releases use '/var/run/nginx.pid'
-        # systemd based distros and Ubuntu 14.04 use '/run/nginx.pid' for their
-        # packages
-        def pidfile_location
-          if (node['nginx']['repo_source'].nil? || %w(distro passenger).include?(node['nginx']['repo_source'])) &&
-            (node['init_package'] == 'systemd' || node['platform_version'].to_f == 14.04)
-            '/run/nginx.pid'
-          else
-            '/var/run/nginx.pid'
-          end
+    module Helpers
+      # pidfile is hard to determine on Debian systems.
+      # Upstream packages and older distro releases use '/var/run/nginx.pid'
+      # systemd based distros and Ubuntu 14.04 use '/run/nginx.pid' for their
+      # packages
+      def pidfile_location
+        if (node['nginx']['repo_source'].nil? || %w(distro passenger).include?(node['nginx']['repo_source'])) && (node['init_package'] == 'systemd' || node['platform_version'].to_f == 14.04)
+          '/run/nginx.pid'
+        else
+          '/var/run/nginx.pid'
         end
       end
     end
