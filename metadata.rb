4ac35f2ae09a589d066d58aea7ab281c46ec1737
Fernando Aleman <fernandoaleman@mac.com>
2019-03-09 11:05:40 -0600

    Formatted metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,18 +1,20 @@
-name 'nginx'
+name             'nginx'
+source_url       'https://github.com/sous-chefs/nginx'
+issues_url       'https://github.com/sous-chefs/nginx/issues'
 maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
-
-license 'Apache-2.0'
-description 'Installs and configures nginx'
+chef_version     '>= 13.3'
+license          'Apache-2.0'
+description      'Installs and configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '9.0.0'
+version          '9.0.0'
 
-recipe 'nginx', 'Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available'
-recipe 'nginx::source', 'Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available'
+recipe   'nginx',           'Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available'
+recipe   'nginx::source',   'Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available'
 
-depends 'build-essential', '>= 5.0'
-depends 'ohai', '>= 4.1.0'
-depends 'yum-epel'
+depends  'build-essential', '>= 5.0'
+depends  'ohai', '>= 4.1.0'
+depends  'yum-epel'
 
 supports 'amazon'
 supports 'centos'
@@ -25,7 +27,3 @@ supports 'ubuntu'
 supports 'suse'
 supports 'opensuse'
 supports 'opensuseleap'
-
-source_url 'https://github.com/sous-chefs/nginx'
-issues_url 'https://github.com/sous-chefs/nginx/issues'
-chef_version '>= 13.3'
