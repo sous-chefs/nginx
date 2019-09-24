0c6f9fb8761353244ba13d2ef36dadc28bae85fc
Zackary Maupin <zmaupin@gmail.com>
2019-09-24 11:25:38 -0400

    nginx: add service resource to nginx_site

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ chef_version     '>= 14'
 license          'Apache-2.0'
 description      'Installs and configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '10.0.1'
+version          '10.0.2'
 
 depends  'ohai', '~> 5.2'
 
