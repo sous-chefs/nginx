64ca3f643cca08d28fca0a28031da823a499b654
Ben Abrams <me@benabrams.it>
2020-08-16 23:43:50 -0700

    move inputs under the verifier config
    Signed-off-by: Ben Abrams <me@benabrams.it>

--- test/integration/install/controls/package_spec.rb
+++ test/integration/install/controls/package_spec.rb
@@ -1,6 +1,8 @@
 control 'package' do
-  desc   'Ensure nginx package is installed.'
+  desc   "Ensure #{input('nginx_package_name')} package is installed."
   impact 1.0
+
+  # puts "nginx: #{input_object('nginx_package_name').diagnostic_string}"
 
   describe package(input('nginx_package_name')) do
     it { should be_installed }
