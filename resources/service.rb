e190a75ad46980277afc96f18b85ff2047e5ac84
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2020-11-25 12:01:36 +0000

    Remove service configuration testing from ruby_block

--- resources/service.rb
+++ resources/service.rb
@@ -35,35 +35,28 @@ property :config_test_fail_action, Symbol,
 action_class do
   def do_service_action(resource_action)
     with_run_context(:root) do
-      if %i(start restart reload).include?(resource_action)
-        edit_resource(:ruby_block, "Run pre #{new_resource.service_name} #{resource_action} configuration test") do
-          block do
-            begin
-              if new_resource.config_test
-                cmd = Mixlib::ShellOut.new("#{nginx_binary} -t -c #{new_resource.config_file}")
-                cmd.run_command.error!
-                Chef::Log.info("Configuration test passed, creating #{new_resource.service_name} #{new_resource.declared_type} resource with action #{resource_action}")
-              else
-                Chef::Log.info("Configuration test disabled, creating #{new_resource.service_name} #{new_resource.declared_type} resource with action #{resource_action}")
-              end
-
-              edit_resource(:service, new_resource.service_name) do
-                action :nothing
-                delayed_action resource_action
-              end
-            rescue Mixlib::ShellOut::ShellCommandFailed
-              if new_resource.config_test_fail_action.eql?(:log)
-                Chef::Log.error("Configuration test failed, #{new_resource.service_name} #{resource_action} action aborted!\n\nError\n-----\n#{cmd.stderr}")
-              else
-                raise "Configuration test failed, #{new_resource.service_name} #{resource_action} action aborted!\n\nError\n-----\n#{cmd.stderr}"
-              end
-            end
+      if %i(start restart reload).include?(resource_action) && ::File.exist?(new_resource.config_file)
+        begin
+          if new_resource.config_test
+            cmd = Mixlib::ShellOut.new("#{nginx_binary} -t -c #{new_resource.config_file}")
+            cmd.run_command.error!
+            Chef::Log.info("Configuration test passed, creating #{new_resource.service_name} #{new_resource.declared_type} resource with action #{resource_action}")
+          else
+            Chef::Log.info("Configuration test disabled, creating #{new_resource.service_name} #{new_resource.declared_type} resource with action #{resource_action}")
           end
 
-          only_if { ::File.exist?(new_resource.config_file) }
-
-          action :nothing
-          delayed_action :run
+          edit_resource(:service, new_resource.service_name) do
+            action :nothing
+            delayed_action resource_action
+          end
+        rescue Mixlib::ShellOut::ShellCommandFailed
+          if new_resource.config_test_fail_action.eql?(:log)
+            Chef::Log.error("Configuration test failed, #{new_resource.service_name} #{resource_action} action aborted!\n\n"\
+                            "Error\n-----\n#{cmd.stderr}")
+          else
+            raise "Configuration test failed, #{new_resource.service_name} #{resource_action} action aborted!\n\n"\
+                  "Error\n-----\nAction: #{resource_action}\n#{cmd.stderr}"
+          end
         end
       else
         edit_resource(:service, new_resource.service_name.delete_suffix('.service')) do
